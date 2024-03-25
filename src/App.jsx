18c321a3026dff9394707eb9e3ee59aff282632d
ceabenshawn <ceasonalgamer@gmail.com>
2024-03-25 22:16:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Apk from "./assets/btp.apk";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+    <div className="container">
+      <div className="content">
+        <p className="title">BEYOND THE <span style={{color:"#ff7b00"}}>PITCH</span></p>
+        <p className="subtitle">What Predictions Will You Check Out Today?</p>
+        <div className="button-container">
+          <a
+            href={Apk}
+            download='BeyondThePitch'
+            target="_blank"
+            rel="noreferrer"
+            className="button"
+          >
+            Download APK
+          </a>
+          <a href="https://btp-website-2k9f.onrender.com/" className="button">
+            Predict On Web
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
